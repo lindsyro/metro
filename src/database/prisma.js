f2671da30013e0785472efadb7561eddf2f700ba
lindsyro <irinarinch@gmail.com>
2026-06-26 21:03:02 +0300

    bot is working

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,15 +1,16 @@
-import { PrismaClient } from '@prisma/client';
-import { Pool } from 'pg';
-import { PrismaPg } from '@prisma/adapter-pg';
-import 'dotenv/config';
+import pkg from "@prisma/client";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import "dotenv/config";
 
-// 1. Создаем пул подключений через классический pg
-const pool = new Pool({ 
-    connectionString: process.env.DATABASE_URL 
+// Достаем PrismaClient из общего пакета
+const { PrismaClient } = pkg;
+
+// Дальше код остается таким же:
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
 });
 
-// 2. Создаем адаптер для Prisma
 const adapter = new PrismaPg(pool);
 
-// 3. Передаем адаптер в клиент
 export const prisma = new PrismaClient({ adapter });
--- src/modules/dbModules.js
+++ src/modules/dbModules.js
@@ -1,4 +1,4 @@
-import { prisma } from '../db/prisma.js';
+import { prisma } from '../database/prisma.js';
 
 // --- ДЛЯ ДИСПЕТЧЕРА (Модуль 4.1) ---
 export async function createIncident(data) {
